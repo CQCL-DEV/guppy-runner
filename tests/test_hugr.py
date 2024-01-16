d8c4595199177cc7aca080d931d656cdb56cc0a5
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-01-16 13:12:10 +0000

    feat: run_guppy_module, and some tests

---
+++ tests/test_hugr.py
@@ -0,0 +1,55 @@
+"""Tests for the public API."""
+
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+from guppy.decorator import guppy
+from guppy.module import GuppyModule
+
+from guppy_runner import run_guppy, run_guppy_module
+
+EVEN_ODD = "test_files/even_odd.py"
+
+
+def test_even_odd():
+    with NamedTemporaryFile(suffix=".hugr") as temp_hugr, NamedTemporaryFile(
+        suffix=".mlir",
+    ) as temp_mlir:
+        temp_hugr.close()
+        temp_mlir.close()
+
+        # Just check that it runs.
+        #
+        # We cannot load any of the artifacts with just the guppy library,
+        # so we have to assume that they are correct.
+        assert run_guppy(
+            EVEN_ODD,
+            hugr_out=Path(temp_hugr.name),
+            mlir_out=Path(temp_mlir.name),
+            # TODO: llvmir generation is broken
+            # llvm_out="test_files/even_odd.ll",  # noqa: ERA001
+            no_run=True,
+        )
+
+
+def test_from_module():
+    module = GuppyModule("module")
+
+    @guppy(module)
+    def main(x: bool) -> bool:  # noqa: FBT001
+        return x
+
+    with NamedTemporaryFile(suffix=".mlir") as temp_mlir:
+        temp_mlir.close()
+
+        # Just check that it runs.
+        #
+        # We cannot load any of the artifacts with just the guppy library,
+        # so we have to assume that they are correct.
+        assert run_guppy_module(
+            module,
+            mlir_out=Path(temp_mlir.name),
+            # TODO: llvmir generation is broken
+            # llvm_out="test_files/even_odd.ll",  # noqa: ERA001
+            no_run=True,
+        )
