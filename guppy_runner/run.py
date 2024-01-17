04974436f25bbd7de0099d6b102a624b480f6611
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-01-17 12:28:18 +0000

    Print the program's output

--- guppy_runner/run.py
+++ guppy_runner/run.py
@@ -10,16 +10,22 @@ from guppy_runner.util import LOGGER
 def run_guppy_bin(binary: Path) -> None:
     """Run the compiled guppy binary."""
     cmd = [
-        binary,
+        binary.absolute(),
     ]
 
     cmd_str = " ".join(str(c) for c in cmd)
     msg = f"Executing command: '{cmd_str}'"
     LOGGER.info(msg)
 
-    subprocess.run(
+    print("----------------------")
+    print("Executing the program:")
+    print()
+
+    out = subprocess.run(  # noqa: PLW1510
         cmd,  # noqa: S603
         capture_output=True,
-        check=True,
-        text=False,
+        # check=True, # This returns a non-zero code  # noqa: ERA001
+        text=True,
     )
+
+    print(out.stdout)
